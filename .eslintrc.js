04dca49e74a78b69abd66ca845d93d00cb571f29
Sebastien Talbot <moi@sebastientalbot.com>
2017-10-12 12:24:21 -0400

    [All] RE-INIT

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
+    },
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'no-unused-vars':'none',
+    'indent': [
+      'warn',
+      2
+    ],
+    'linebreak-style': [
+      'warn',
+      'unix'
+    ],
+    'quotes': [
+      'warn',
+      'single'
+    ],
+    'semi': [
+      'warn',
+      'always'
+    ]
+  }
+};
